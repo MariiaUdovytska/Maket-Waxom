753e1319ff43ca6df31c8bc2645e1377f8800db0
MariiaUdovytska <mariiaudovytska06@gmail.com>
2021-07-16 13:16:27 +0300

    block 1 with adaptive

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/*
+function digitize(n) {
+	//code here
+	let myArray = n.toString().split('').map(int => parseInt(int));
+	return myArray.reverse();
+}
+
+console.log(digitize(348597));
+*/
+
+/*
+var replaceDots = function (str) {
+	return str.replace(/\./g, "-");
+}
+
+console.log(replaceDots("eeggg.gghg.gjyjy"));
+*/
+
+/*
+function squareSum(numbers) {
+	let squared = [];
+	for (let i = 0; i < numbers.length; i++) {
+		squared.push(Math.pow(numbers[i], 2));
+	}
+	let summ = 0
+	for (let i = 0; i < numbers.length; i++) {
+		summ += squared[i];
+	}
+
+	return summ;
+}
+
+console.log(squareSum([0, 3, 4, 5]))
+*/
